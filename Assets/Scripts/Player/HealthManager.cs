0c76c9c8193acba76c359436dd5ffcea896c527f
loyet <thomas.loye@etu.umontpellier.fr>
2025-01-24 15:46:56 +0100

    feat : coeurs dans l'UI

---
+++ Assets/Scripts/Player/HealthManager.cs
@@ -0,0 +1,17 @@
+﻿using Unity.VisualScripting;
+
+namespace Player
+{
+    public class HealthManager
+    {
+        public float health;
+
+        public void Start()
+        {
+            health = 3;
+            
+            //On ajoute trois coeurs sur l'ui
+            
+        }
+    }
+}
