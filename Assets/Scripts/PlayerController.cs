b112d171a2f4834fde91cf513eafdf37afb1c703
Lorick Vergnes <lorick.vergnes@etu.umontpellier.fr>
2025-01-24 12:19:31 +0100

    feat : movenemt player right left and jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,44 @@
-﻿namespace DefaultNamespace;
+﻿using UnityEngine;
 
-public class PlayerController
+public class PlayerController : MonoBehaviour
 {
-    
+    public float speed = 5f;
+
+    private Rigidbody2D rb; 
+    private Vector2 movement; 
+    public float jumpingPower;
+    private bool shouldJump;
+
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    float Horizontal;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        movement = new Vector2(horizontalInput, 0);
+        if (isGrounded() && Input.GetKeyDown(KeyCode.Space))
+    {
+        shouldJump = true;
+    }
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = movement * speed;
+        if(shouldJump){
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
+            shouldJump = false;
+        }
+    }
+
+    bool isGrounded()
+    {
+        return Physics2D.OverlapCapsule(groundCheck.position, new Vector2(1f, .1f), CapsuleDirection2D.Horizontal, 0, groundLayer);
+    }
 }
