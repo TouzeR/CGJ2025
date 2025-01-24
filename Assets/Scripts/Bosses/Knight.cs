b96347641c5316e18f0affd78fdbee5add43f70c
TouzeR <romain.touze@etu.umontpellier.fr>
2025-01-24 10:59:44 +0100

    feat Boss : init Knight Boss and random attack

---
+++ Assets/Scripts/Bosses/Knight.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Unity.VisualScripting;
+using UnityEngine;
+using Random = System.Random;
+
+namespace Bosses
+{
+    public class Knight : Boss
+    {
+        private bool inCooldown = false;
+        public int cooldown;
+        
+        List<int> listIntruments = new List<int>();
+
+        void Start()
+        {
+            health = 10;
+            listIntruments.Add(1);
+            listIntruments.Add(2);
+        }
+
+        void Update()
+        {
+            base.Update();
+        }
+        
+        protected async override void Attack()
+        {
+            if (inCooldown) return;
+
+            inCooldown = true;
+
+            Random rnd = new Random();
+            int attack = rnd.Next(listIntruments.Count);
+            
+            await Task.Delay(cooldown);
+            inCooldown = false;
+
+        }
+    }
+}
