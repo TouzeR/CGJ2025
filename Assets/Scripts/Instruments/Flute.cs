06d31279f398612d92bf1c60246d58f375045e56
loyet <thomas.loye@etu.umontpellier.fr>
2025-01-24 09:42:12 +0100

    feat : init des instruments Trompette et Flute

---
+++ Assets/Scripts/Instruments/Flute.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Instruments
+{
+    public class Flute : Instrument
+    {
+        private new string name = "Flûte";
+        private AudioClip sound;
+        private Sprite image;
+        private new KeyCode key = KeyCode.F;
+        
+        /**
+         * On attribue les bonnes valeurs aux variables de la classe
+         */
+        public new void Start()
+        {
+
+        }
+        
+        public new void Stop()
+        {
+            // Stop the flute
+        }
+        
+        public new void Update()
+        {
+            // Update the flute
+        }
+        
+        
+        
+    }
+}
---
+++ Assets/Scripts/Instruments/Instrument.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Instruments
+{
+    public class Instrument
+    {
+        private string name;
+        private AudioClip sound;
+        private Image image;
+        private KeyCode key;
+    
+    
+        public void Start()
+        {
+            // Start the instrument
+        }
+    
+        public void Stop()
+        {
+            // Stop the instrument
+        }
+    
+        public void Update()
+        {
+            // Update the instrument
+        }
+    
+    
+    }
+}
