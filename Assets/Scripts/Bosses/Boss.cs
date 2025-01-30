1c301ce2d9b28905e8fb98750e0ccbbbd86a43b0
loyet <thomas.loye@etu.umontpellier.fr>
2025-01-30 23:13:37 +0100

    feat : héritage boss + GameplayManager. Reste à corriger respawn des boss

--- Assets/Scripts/Bosses/Boss.cs
+++ Assets/Scripts/Bosses/Boss.cs
@@ -1,22 +1,20 @@
 using System;
-using System.Collections.Generic;
 using System.IO.Pipes;
 using Player;
 using UnityEngine;
 using System.Threading.Tasks;
-using Instruments;
 using TMPro;
 using UnityEngine.UI;
-using Random = System.Random;
 
-public class Boss : MonoBehaviour
+public abstract class Boss : MonoBehaviour
 {
     public int health;
     public int maxHealth;
     public HealthBar healthBar;
+    public float damage;
     private HealthManager HealthManager;
     private bool isRespawning = false;
-    protected int level = 1;
+    public int level = 1;
     public TextMeshProUGUI score;
 
     public Animator animator;
@@ -25,47 +23,10 @@ public class Boss : MonoBehaviour
     public Image wrapLevel;
     public TextMeshProUGUI levelText;
 
-    private bool inCooldown = false;
-    private int cooldown;
-    private bool isAttacking = false;
 
-    // La liste des classes d'instruments
-    public Flute flute;
-    public Trompette trompette;
-    public Harpe harpe;
-    public Violon violon;
-    public Banjo banjo;
-    public Ocarina ocarina;
-
-    private List<Instrument> instruments = new List<Instrument>();
-
-    private HealthManager healthManager;
-
-
-    //public Level level;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    protected async void Start()
+    
+    public void Start()
     {
-        
-        maxHealth = 3;
-        instruments.Add(flute);
-        instruments.Add(trompette);
-        instruments.Add(harpe);
-        instruments.Add(violon);
-        instruments.Add(banjo);
-        instruments.Add(ocarina);
-
-        isRespawning = false;
-        Random rnd = new Random();
-        cooldown = rnd.Next(6000, 12000);
-
-        healthManager = FindFirstObjectByType(typeof(HealthManager)) as HealthManager;
-
-        if (healthManager == null)
-        {
-            Debug.LogError("healthManager n'a pas été trouvé dans la scène.");
-        }
-
         HealthManager = FindFirstObjectByType(typeof(HealthManager)) as HealthManager;
         health = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
@@ -74,41 +35,50 @@ public class Boss : MonoBehaviour
         wrapLevel.enabled = false;
         levelText.enabled = false;
         DisplayLevel();
+
     }
 
-    // Update is called once per frame
-    protected void Update()
+    protected async void Update()
     {
-        if (health > 0 && HealthManager.health > 0)
-        {
-            Attack();
-        }
-        else
-        {
-            if (!isRespawning && HealthManager.health > 0)
-            {
-                animator.SetBool("isDead", true);
-                isRespawning = true;
-                Respawn();
-            }
-        }
-    }
-
-    private async void Respawn()
+    if (health > 0 && HealthManager.health > 0)
     {
-        particleSystem.Stop();
-        await Task.Delay(6000);
-        health = maxHealth + level;
-        level++;
-        score.text = (level - 1).ToString();
-        healthBar.SetMaxHealth(health);
-        healthBar.SetHealth(health);
-        isRespawning = false;
-        animator.SetBool("isDead", false);
-        DisplayLevel();
+        Attack();
     }
+    else
+    {
+        if (!isRespawning)
+        {
+            animator.SetBool("isDead", true);
+            isRespawning = true;
+            //Respawn();
+            particleSystem.Stop();
+            await Task.Delay(5000);
+            score.text = (level - 1).ToString();
+            healthBar.SetMaxHealth(health);
+            healthBar.SetHealth(health);
+            isRespawning = false;
+            animator.SetBool("isDead", false);
+            DisplayLevel();
+        }
+    }
+}
 
-    public void Reset()
+private async void Respawn()
+{
+    particleSystem.Stop();
+    await Task.Delay(5000);
+    health = maxHealth + level;
+    level ++;
+    score.text = (level - 1).ToString();
+    healthBar.SetMaxHealth(health);
+    healthBar.SetHealth(health);
+    isRespawning = false;
+    animator.SetBool("isDead", false);
+    DisplayLevel();
+
+}
+
+public void Reset()
     {
         particleSystem.Stop();
         health = maxHealth;
@@ -118,43 +88,12 @@ public class Boss : MonoBehaviour
         healthBar.SetHealth(health);
         Debug.Log("Boss Reset" + health);
         DisplayLevel();
+
     }
 
-    protected async void Attack()
-    {
-        if (inCooldown) return;
-
-        inCooldown = true;
-
-        Random rnd = new Random();
-        int attack = rnd.Next(instruments.Count);
-        animator.SetBool("isAttacking", true);
-        instruments[attack].PlaySound();
-        Debug.Log("Attaque boss : " + instruments[attack].getKey());
-        bool playerRespondedCorrectly = await WaitForPlayerResponse(instruments[attack].getKey());
-
-        if (playerRespondedCorrectly)
-        {
-            particleSystem.Play();
-            health -= 1;
-            healthBar.SetHealth(health);
-            Debug.Log("Health du boss : " + health);
-            animator.SetBool("isAttacking", false);
-        }
-        else
-        {
-            healthManager.TakeDamage();
-            animator.SetBool("isAttacking", false);
-        }
-
-        await Task.Delay(500);
-        particleSystem.Stop();
-
-        await Task.Delay(cooldown);
-        inCooldown = false;
-    }
-
-
+    protected abstract void Attack();
+    
+    
     public async void DisplayLevel()
     {
         wrapLevel.enabled = true;
@@ -164,30 +103,5 @@ public class Boss : MonoBehaviour
         wrapLevel.enabled = false;
         levelText.enabled = false;
     }
-
-    private async Task<bool> WaitForPlayerResponse(KeyCode expectedKey)
-    {
-        bool playerResponded = false;
-        float timeLimit = 6f;
-        float elapsedTime = 0f;
-
-        while (elapsedTime < timeLimit)
-        {
-            if (Input.GetKeyDown(expectedKey))
-            {
-                playerResponded = true;
-                break;
-            }
-
-            if (Input.anyKeyDown && !Input.GetKeyDown(expectedKey))
-            {
-                break;
-            }
-
-            await Task.Yield();
-            elapsedTime += Time.deltaTime;
-        }
-
-        return playerResponded;
-    }
 }
+
---
+++ Assets/Scripts/GameplayManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Bosses;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class GameplayManager : MonoBehaviour
+    {
+        private Boss knight;
+        private Boss dragon;
+        private Boss bossActuel;
+        
+        void Start()
+        {
+            knight = FindFirstObjectByType(typeof(Knight)) as Knight;
+            //dragon = FindFirstObjectByType(typeof(Dragon)) as Dragon;
+            dragon = null; //TODO: à enlever pour utiliser vraiment le dragon une fois fini
+
+            //bossActuel = Random.Range(0, 2) == 0 ? knight : dragon; TODO : changer lors de la mise en place du dragon
+            bossActuel = knight;
+            
+            DemarrerPartieBoss(); //TODO : changer lors de la mise en place des blocks
+        }
+        
+        void Update()
+        {
+            if (bossActuel.health <= 0)
+            {
+                int level = bossActuel.level;
+                int maxHealth = bossActuel.maxHealth;
+                //bossActuel = bossActuel == knight ? dragon : knight; //TODO : changer lors de la mise en place du dragon
+                bossActuel.level = level + 1;
+                bossActuel.health = maxHealth + level;
+            }
+            
+        }
+
+        void DemarrerPartieBoss()
+        {
+            bossActuel.Start();
+        }
+        
+        
+        void DemarrerPartieBlocks()
+        {
+            throw new NotImplementedException("TODO !");
+        }
+    }
+}
